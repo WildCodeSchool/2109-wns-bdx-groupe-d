b77211a6559b23837c0a37e643309e661fe1b160
AxelFasst <axel.cannit@fasst.io>
2021-10-29 09:51:26 +0200

    create a simple react app

---
+++ api/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coucou les p'tits loups!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
