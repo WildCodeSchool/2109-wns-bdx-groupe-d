c284892e5a70656f2bd58b645f70434f41cf1764
AxelFasst <axel.cannit@fasst.io>
2021-11-26 12:29:12 +0100

    push for work on same base for resolve bug

--- server/src/spec/User.unitary.spec.ts
+++ server/src/spec/User.unitary.spec.ts
@@ -7,55 +7,89 @@ import User from '../models/User';
 
 describe('UserResolver', () => {
     let server: ApolloServer;
+
     beforeAll(async () => {
         server = await getApolloServer();
     });
+
     beforeEach(() => getDatabaseConnection());
+
     afterEach(() => getConnection().close());
-    describe('CreateUser', () => {
-        const createUserData = `
-            mutation($first_name: String!, $last_name: String!, roles: String!, email: String!, password: String!, color_id: Number!, created_at: String!) {
-                user(first_name, last_name, roles, email, password, color_id, created_at) {
-                    id
-                    first_name
-                    last_name
-                    roles
-                    email
-                    password
-                    color_id
-                    created_at
-                }
-            }`;
 
-        it('create a use in database', async () => {
+    describe("query users", () => {
+        const getUsers = `
+        query {
+          Users {
+            id
+            first_name
+            last_name
+            roles
+            email
+            password
+            color_id
+            organization_id
+            created_at
+          }
+        }`;
+    
+        describe("when there are no users in database", () => {
+          it("returns empty array", async () => {
             const result = await server.executeOperation({
-                query: createUserData,
-                variables: {
-                    first_name: "Gros",
-                    last_name: "Tony",
-                    roles: `['ROLE_USER']`,
-                    email: 'azerty@poiuy.com',
-                    password: 'root',
-                    color_id: 9,
-                    created_at: "2016-07-20T17:30:15+05:30"
-                },
+              query: getUsers,
             });
-
-            expect(await User.findOne({ first_name: "Gros" })).toHaveProperty(
-                "last_name",
-                "Tony"
-            );
-
             expect(result.errors).toBeUndefined();
-            expect(result.data?.createUserData).toEqual({
-                first_name: "Gros",
-                last_name: "Tony",
-                roles: `['ROLE_USER']`,
-                email: 'azerty@poiuy.com',
-                password: 'root',
-                color_id: 9,
-                created_at: "2016-07-20T17:30:15+05:30"
-            })
-        })
+            expect(result.data?.wilders).toEqual([]);
+          });
+        });
+
+    // describe('CreateUser', () => {
+    //     const createUserData = `
+    //         mutation($first_name: String!, $last_name: String!, $roles: String!, $email: String!, $password: String!, $color_id: String!, $organization_id: String!, $created_at: String!) {
+    //             createUser(first_name: $first_name, last_name: $last_name, roles: $roles, email: $email, password: $password, color_id: $color_id, organization_id: $organization_id, created_at: $created_at) {
+    //                 id
+    //                 first_name
+    //                 last_name
+    //                 roles
+    //                 email
+    //                 password
+    //                 color_id
+    //                 organization_id
+    //                 created_at
+    //             }
+    //         }`;
+
+    //     it('create a use in database', async () => {
+    //         const result = await server.executeOperation({
+    //             query: createUserData,
+    //             variables: {
+    //                 first_name: "Gros",
+    //                 last_name: "Tony",
+    //                 roles: `['ROLE_USER']`,
+    //                 email: 'azerty@poiuy.com',
+    //                 password: 'root',
+    //                 color_id: '9',
+    //                 organization_id: '9',
+    //                 created_at: "2016-07-20T17:30:15+05:30"
+    //             },
+    //         });
+
+            // console.log(result);
+
+            // expect(await User.findOne({ first_name: "Gros" })).toHaveProperty(
+            //     "last_name",
+            //     "Tony"
+            // );
+
+            // expect(result.errors).toBeUndefined();
+            // expect(result.data?.createUserData).toEqual({
+            //     first_name: "Gros",
+            //     last_name: "Tony",
+            //     roles: `['ROLE_USER']`,
+            //     email: 'azerty@poiuy.com',
+            //     password: 'root',
+            //     color_id: 9,
+            //     created_at: "2016-07-20T17:30:15+05:30"
+            // })
+    //     })
     })
 })
