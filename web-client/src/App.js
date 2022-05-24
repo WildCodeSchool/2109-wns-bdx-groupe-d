c2a6086c7d14f348611a497253c7f9277773dd37
clarade <cdesperben@yahoo.fr>
2022-05-24 15:23:03 +0200

    implemented layout in app

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { useQuery } from "@apollo/client";
-import { userInfo } from "./graphql/UserSession.js";
+import { useQuery } from '@apollo/client';
+import { userInfo } from './graphql/UserSession.js';
 
 import Settings from './pages/Settings.js';
 import Dashboard from './pages/Dashboard.js';
@@ -12,40 +12,46 @@ import TopBar from './components/TopBar.js';
 import Login from './pages/Login.js';
 import './css/tailwind.css';
 import Subsription from './pages/Subscription.js';
+import Layout from './components/Layout.js';
+
 const App = () => {
 	const { data, refetch } = useQuery(userInfo);
 
 	return (
 		<div className="w-full">
-			{!data ? <>
-				<Route exact path="/">
-					<Login onLoginSuccess={refetch}/>
-				</Route>
-				<Route exact path="/subscription">
-					<Subsription />
-				</Route>
-			</>
-			: <>
-			 	<TopBar />
-				<Switch>
+			{!data ? (
+				<>
 					<Route exact path="/">
-						<Dashboard actualUser={data.userInfo} />
+						<Login onLoginSuccess={refetch} />
 					</Route>
-					<Route path="/organization">
-						<Organization />
+					<Route exact path="/subscription">
+						<Subsription />
 					</Route>
-					<Route path="/projects">
-						<Projects />
-					</Route>
-					<Route path="/issues">
-						<Issues />
-					</Route>
-					<Route path="/settings">
-						<Settings />
-					</Route>
-				</Switch>
-			</>
-			}
+				</>
+			) : (
+				<>
+					<TopBar />
+					<Layout>
+						<Switch>
+							<Route exact path="/">
+								<Dashboard actualUser={data.userInfo} />
+							</Route>
+							<Route path="/organization">
+								<Organization />
+							</Route>
+							<Route path="/projects">
+								<Projects />
+							</Route>
+							<Route path="/issues">
+								<Issues />
+							</Route>
+							<Route path="/settings">
+								<Settings />
+							</Route>
+						</Switch>
+					</Layout>
+				</>
+			)}
 		</div>
 	);
 };
