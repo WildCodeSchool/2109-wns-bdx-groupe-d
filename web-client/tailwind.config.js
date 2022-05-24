6b03080042d017f157e4123d3a0d2634856c2151
clarade <cdesperben@yahoo.fr>
2022-05-24 16:03:04 +0200

    changed text color class name standard for more explicit one

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        wildmine_black: '#171A21',
-        secondary_color: '#5A9178',
-        secondary_color_hover: '#0F766E',
-        text_color: '#E7E7E7'
-      }
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				wildmine_black: '#171A21',
+				secondary_color: '#5A9178',
+				secondary_color_hover: '#0F766E',
+				text_color_light: '#E7E7E7',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
