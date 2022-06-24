dc86860bc9f45ddbd6fc094bfa2cbf24de04eb95
clarade <cdesperben@yahoo.fr>
2022-06-24 16:25:06 +0200

    added logic for the scroll action and the display of the navbar background

--- web-client/src/components/TopBar.js
+++ web-client/src/components/TopBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { userInfo } from '../graphql/UserSession';
@@ -7,12 +7,38 @@ import '../css/components/TopBar.css';
 
 const TopBar = () => {
 	const [currentLoc, setCurrentLoc] = useState(window.location.pathname);
+	const [navbar, setNavbar] = useState(false);
+	// const [element, setElement] = useState();
 	const { data } = useQuery(userInfo);
 	const currentUser = data.userInfo;
-	return (
-		<nav className="topbar-container">
-			<div className="logo-container">LOGO</div>
 
+	// const changeNavBackground = () => {
+	// 	window.scrollY >= 100 ? setNavbar(true) : setNavbar(false);
+	// };
+
+	// if(window.scrollY >= 100) {
+	// 	setNavbar(true)
+	// } else {
+	// 	setNavbar(false)
+	// };
+
+	// window.addEventListener('scroll', changeNavBackground());
+
+	// useEffect(() => {
+	// 	window.addEventListener('scroll', changeNavBackground);
+	// }, []);
+
+	window.onscroll = () => {
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	};
+
+	return (
+		<nav className={`navbar ${navbar ? 'navActive' : 'navInactive'}`}>
+			<div className="logo-container">LOGO</div>
 			<div className="topbar-menu-links-container">
 				<NavLink
 					to="/"
@@ -46,7 +72,6 @@ const TopBar = () => {
 					Tickets
 				</NavLink>
 			</div>
-
 			<div className="topbar-menu-settings">
 				<NavLink to="/settings" onClick={() => setCurrentLoc('/settings')}>
 					{currentUser.first_name} {currentUser.last_name}
