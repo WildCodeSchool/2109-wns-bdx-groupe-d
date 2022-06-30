3053a578b58543f207f8f2ac922abfc6e16dd8c8
AxelFasst <axel.cannit@fasst.io>
2022-06-30 10:13:58 +0200

    repair session and modify graphql function for send user with relations in a special query

--- web-client/src/pages/IssuesProject.js
+++ web-client/src/pages/IssuesProject.js
@@ -4,13 +4,13 @@ import { useQuery } from "@apollo/client";
 import SearchButton from '../components/SearchButton';
 import DisplayIssuesTitle from './components/issues/DisplayIssuesTitle';
 import DisplayIssuesValues from './components/issues/DisplayIssuesValues';
-import { getMyIssues } from '../graphql/Issue';
+import { userWithRelations } from '../graphql/UserSession';
 
 const IssuesProject = () => {
-	const { loading, data } = useQuery(getMyIssues);
+	const { loading, data } = useQuery(userWithRelations);
 
   if (loading) return <div className='mx-auto'>Chargement...</div>
-
+  
   return <div>
     <div className='w-[230px]'>
       <SearchButton/>
@@ -19,9 +19,11 @@ const IssuesProject = () => {
     <DisplayIssuesTitle/>
 
     <div>
-      {data.getMyIssues.map((issue, issueIndex) => (
-          <DisplayIssuesValues key={issueIndex} issue={issue} issueIndex={issueIndex} issues={data.getMyIssues}/>
-      ))}
+      {data.userWithRelations.issues_assigned
+      ? data.userWithRelations.issues_assigned.map((issue, issueIndex) => (
+          <DisplayIssuesValues key={issueIndex} issue={issue} issueIndex={issueIndex} issues={data.userWithRelations.issues_assigned}/>
+      ))
+    : <p>Aucun ticket ne vous est assigné pour le moment</p>}
     </div>
   </div>;
 };
