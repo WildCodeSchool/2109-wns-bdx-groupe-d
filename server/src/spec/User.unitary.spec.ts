93668d9489b26a5388f069a76afb9b97ae217348
clarade <cdesperben@yahoo.fr>
2022-11-28 11:56:31 +0100

    change modal for user update

--- server/src/spec/User.unitary.spec.ts
+++ server/src/spec/User.unitary.spec.ts
@@ -47,54 +47,54 @@ describe('UserResolver', () => {
 			});
 		});
 
-		// describe('CreateUser', () => {
-		//     const createUserData = `
-		//         mutation($first_name: String!, $last_name: String!, $roles: String!, $email: String!, $password: String!, $color_id: String!, $organization_id: String!, $created_at: String!) {
-		//             createUser(first_name: $first_name, last_name: $last_name, roles: $roles, email: $email, password: $password, color_id: $color_id, organization_id: $organization_id, created_at: $created_at) {
-		//                 id
-		//                 first_name
-		//                 last_name
-		//                 roles
-		//                 email
-		//                 password
-		//                 color_id
-		//                 organization_id
-		//                 created_at
-		//             }
-		//         }`;
+		describe('CreateUser', () => {
+		    const createUserData = `
+		        mutation($first_name: String!, $last_name: String!, $roles: String!, $email: String!, $password: String!, $color_id: String!, $organization_id: String!, $created_at: String!) {
+		            createUser(first_name: $first_name, last_name: $last_name, roles: $roles, email: $email, password: $password, color_id: $color_id, organization_id: $organization_id, created_at: $created_at) {
+		                id
+		                first_name
+		                last_name
+		                roles
+		                email
+		                password
+		                color_id
+		                organization_id
+		                created_at
+		            }
+		        }`;
 
-		//     it('create a use in database', async () => {
-		//         const result = await server.executeOperation({
-		//             query: createUserData,
-		//             variables: {
-		//                 first_name: "Gros",
-		//                 last_name: "Tony",
-		//                 roles: `['ROLE_USER']`,
-		//                 email: 'azerty@poiuy.com',
-		//                 password: 'root',
-		//                 color_id: '9',
-		//                 organization_id: '9',
-		//                 created_at: "2016-07-20T17:30:15+05:30"
-		//             },
-		//         });
+		    it('create a use in database', async () => {
+		        const result = await server.executeOperation({
+		            query: createUserData,
+		            variables: {
+		                first_name: "Gros",
+		                last_name: "Tony",
+		                roles: `['ROLE_USER']`,
+		                email: 'azerty@poiuy.com',
+		                password: 'root',
+		                color_id: '9',
+		                organization_id: '9',
+		                created_at: "2016-07-20T17:30:15+05:30"
+		            },
+		        });
 
-		// console.log(result);
+		console.log(result);
 
-		// expect(await User.findOne({ first_name: "Gros" })).toHaveProperty(
-		//     "last_name",
-		//     "Tony"
-		// );
+		expect(await User.findOne({ first_name: "Gros" })).toHaveProperty(
+		    "last_name",
+		    "Tony"
+		);
 
-		// expect(result.errors).toBeUndefined();
-		// expect(result.data?.createUserData).toEqual({
-		//     first_name: "Gros",
-		//     last_name: "Tony",
-		//     roles: `['ROLE_USER']`,
-		//     email: 'azerty@poiuy.com',
-		//     password: 'root',
-		//     color_id: 9,
-		//     created_at: "2016-07-20T17:30:15+05:30"
-		// })
-		//     })
+		expect(result.errors).toBeUndefined();
+		expect(result.data?.createUserData).toEqual({
+		    first_name: "Gros",
+		    last_name: "Tony",
+		    roles: `['ROLE_USER']`,
+		    email: 'azerty@poiuy.com',
+		    password: 'root',
+		    color_id: 9,
+		    created_at: "2016-07-20T17:30:15+05:30"
+		})
+		    })
 	});
-});
+})});
